876943cf85fd5e11c077e4521754db1e72286005
Karoli Kolokonyi <karoli@kepler.org>
2017-05-02 19:55:36 +0200

    added User controller and Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ 
+  get "/login", to: 'sessions#new'
+  post "/login", to: 'sessions#create'
+  delete "/logout", to: 'sessions#destroy'
+
+
+  devise_for :users, :skip => [:registrations] as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+
+  end
+end 
