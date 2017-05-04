f545b810a1666b5db45655ae7c04e3ffe8d3c87d
Gedeon Ntwali <ntwari28@gmail.com>
2017-05-04 18:52:24 +0200

    Controller and index page of cfa

--- app/controllers/competencies_controller.rb
+++ app/controllers/competencies_controller.rb
@@ -1,2 +1,12 @@
 class CompetenciesController < ApplicationController
+
+  def index
+    @competencies = Unirest.get(CFAAPI)
+  end
+
+  def show
+    @competency = Unirest.get(CFAAPI)
+  end
+
+
 end
